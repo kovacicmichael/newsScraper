98c624660d7fdd7e7b2d56687078080e3dc79124
Michael Kovacic <michael.j.kovacic@gmail.com>
2018-03-19 20:15:35 -0600

    route fix

--- routes-controller/html-routes.js
+++ routes-controller/html-routes.js
@@ -4,19 +4,17 @@ var path = require("path");
 module.exports = function(app){
 
 
-app.get("/", function(req, res){
+// app.get("/", function(req, res){
 
-	res.sendFile('index.html', { root: path.join(__dirname, '../public') });
-})
+// 	console.log("index.html")
+// 	res.sendFile('index.html', { root: path.join(__dirname, '../public') });
+// })
 
 app.get("/savedArticles", function(req, res){
 	console.log("html rendering")
 
 	res.sendFile('article.html', { root: path.join(__dirname, '../public') });
 })
-
-
-
 
 
 
