4888a5b973df0cd289854cc92dc4a98adb4e0945
Michael Kovacic <michael.j.kovacic@gmail.com>
2018-03-19 20:18:19 -0600

    route change

--- routes-controller/html-routes.js
+++ routes-controller/html-routes.js
@@ -4,11 +4,11 @@ var path = require("path");
 module.exports = function(app){
 
 
-// app.get("/", function(req, res){
+app.get("/", function(req, res){
 
-// 	console.log("index.html")
-// 	res.sendFile('index.html', { root: path.join(__dirname, '../public') });
-// })
+	console.log("index.html")
+	res.sendFile('index.html', { root: path.join(__dirname, '../public') });
+})
 
 app.get("/savedArticles", function(req, res){
 	console.log("html rendering")
