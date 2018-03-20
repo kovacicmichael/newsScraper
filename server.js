7a08ce26b8027c7c55d687ddd46a55440947e665
Michael Kovacic <michael.j.kovacic@gmail.com>
2018-03-19 19:32:51 -0600

    port changes

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -47,6 +47,6 @@ require("./routes-controller/html-routes.js")(app);
 
 
 
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
 });
